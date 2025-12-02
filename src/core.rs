6d6901a9e63bfe7cdcc4e1d328ddbb7ed1a86e79
Ryder Freeman <RyderFreeman4Logos@gmail.com>
2025-12-02 14:20:12 -0800

    feat(core): edit the Domain Separator

--- src/core.rs
+++ src/core.rs
@@ -2,7 +2,7 @@ use blake3::Hasher as Blake3Hasher;
 
 pub fn derive_challenge(master_challenge: [u8; 32], proof_id: u64) -> [u8; 32] {
     let mut hasher = Blake3Hasher::new();
-    hasher.update(b"rspow:equix:challenge:v1|");
+    hasher.update(b"rspow:challenge:v1|");
     hasher.update(&master_challenge);
     hasher.update(&proof_id.to_le_bytes());
     hasher.finalize().into()
